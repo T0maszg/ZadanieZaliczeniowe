1d44f24cd487552224c88d5b3c0b60234f2fb378
Tomasz <tomaszbochenek04@gmail.com>
2025-12-03 13:01:34 +0100

    Prawie gotowy wygląd

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -27,40 +27,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        EditText imie = findViewById(R.id.name);
-        EditText email = findViewById(R.id.mail);
-        EditText password=findViewById(R.id.password);
-        EditText passwordagain = findViewById(R.id.passwordagain);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-                if (imie.getText().toString().equals("")||email.getText().toString().equals("")||password.getText().toString().equals("") || passwordagain.getText().toString().equals("")){
-                    Toast.makeText(MainActivity.this, "Wypełniej każde pole", Toast.LENGTH_SHORT).show();
-                }
-                String email = ((EditText)findViewById(R.id.mail)).getText().toString();
-                String expression = "^[a-zA-Z0-9+_.-]+@[a-z]+\\.+[a-z]+";
-                Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-                String password=((EditText)findViewById(R.id.password)).getText().toString();
-                String passwordagain=((EditText)findViewById(R.id.passwordagain)).getText().toString();
-
-                Matcher matcher = pattern.matcher(email);
-                if(matcher.matches()){
-                    Toast.makeText(MainActivity.this, "Dobrze wpisany email", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this, "Zle wpisany email", Toast.LENGTH_SHORT).show();
-                }
-                if (password.equals(passwordagain)&& password.length()>=6){
-                    Toast.makeText(MainActivity.this, "Dobre hasło", Toast.LENGTH_SHORT).show();
-
-                }else{
-                    Toast.makeText(MainActivity.this, "Złe hasło", Toast.LENGTH_SHORT).show();
-                }
-
-
-            }
-        });
     }
 }
